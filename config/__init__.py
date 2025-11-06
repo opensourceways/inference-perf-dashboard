8c27705a0c2e83aa8d93719d93f9ab144c22042b
qinsir5522 <15769255522@163.com>
2025-11-06 09:54:24 +0800

    Add '__init__.py' file to optimize package import paths.

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-from config.logger_config import get_logger
-
-__all__ = ["get_logger"]
--- es_command/__init__.py
+++
@@ -1,3 +0,0 @@
-from es_command.es_operation import init_es_operation, ESHandler
-
-__all__ = ['init_es_operation', 'ESHandler']
